f1ee115f6cd440453548fbfe98f683d482e5e478
Lelele <unrequired@163.com>
2021-01-29 05:55:31 +0000

    feat(zset): z range by score

--- zset.go
+++ zset.go
@@ -57,6 +57,21 @@ func (z *zSetRds) ZRange(key string, start, stop interface{}, withScore ...bool)
 	return getReply(c.Do("zrange", key, start, stop))
 }
 
+func (z *zSetRds) ZRangeByScore(key string, start, stop interface{}, args ...interface{}) *Reply {
+	// ZRANGEBYSCORE key min max [WITHSCORES] [LIMIT offset count]
+	c := pool.Get()
+	defer c.Close()
+
+	if len(args) == 1 {
+		return getReply(c.Do("zrangebyscore", key, start, stop, args[0]))
+	}
+	if len(args) == 2 {
+		return getReply(c.Do("zrangebyscore", key, start, stop, args[0], args[1]))
+	}
+	return getReply(c.Do("zrangebyscore", key, start, stop))
+}
+
+
 // 倒序返回指定区间内的元素
 func (z *zSetRds) ZRevrange(key string, start, stop interface{}, withScore ...bool) *Reply {
 	c := pool.Get()
